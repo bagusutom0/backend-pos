b7a8a6f4af938f35a389892d51f65cbeaa3623ed
bagusutom0 <bagussajiwoutomo12@gmail.com>
2024-11-26 19:25:46 +0700

    tugas 12-framework-bagus

--- backend/tugas-12/gen-24-java-spring-framework/src/main/java/com/bagus/gen_24_java_spring_framework/controller/HomeController.java
+++ backend/tugas-12/gen-24-java-spring-framework/src/main/java/com/bagus/gen_24_java_spring_framework/controller/HomeController.java
@@ -1,6 +1,6 @@
 package com.bagus.gen_24_java_spring_framework.controller;
 
-import lombok.Getter;
+import com.bagus.gen_24_java_spring_framework.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+    private final ProductService service;
 
     @GetMapping
     public String home(Model model) {
+        service.saveProducts();
+        model.addAttribute("products", service.getAllProducts());
         return "index";
     }
 }
--- backend/tugas-12/gen-24-java-spring-framework/src/main/java/com/bagus/gen_24_java_spring_framework/entity/Product.java
+++ backend/tugas-12/gen-24-java-spring-framework/src/main/java/com/bagus/gen_24_java_spring_framework/entity/Product.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     @Id
     private Long id;

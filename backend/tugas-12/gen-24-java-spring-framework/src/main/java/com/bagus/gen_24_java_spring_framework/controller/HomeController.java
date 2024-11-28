70e667513903009cbb752370db37a1504103e31d
bagusutom0 <bagussajiwoutomo12@gmail.com>
2024-11-28 11:10:38 +0700

    tugas 12-framework-bagus

--- backend/tugas-12/gen-24-java-spring-framework/src/main/java/com/bagus/gen_24_java_spring_framework/controller/HomeController.java
+++ backend/tugas-12/gen-24-java-spring-framework/src/main/java/com/bagus/gen_24_java_spring_framework/controller/HomeController.java
@@ -1,10 +1,16 @@
 package com.bagus.gen_24_java_spring_framework.controller;
 
+import com.bagus.gen_24_java_spring_framework.entity.Product;
 import com.bagus.gen_24_java_spring_framework.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,8 +19,44 @@ public class HomeController {
 
     @GetMapping
     public String home(Model model) {
-        service.saveProducts();
         model.addAttribute("products", service.getAllProducts());
         return "index";
+    }
+
+    @GetMapping("/save")
+    public String createProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "save";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateProduct(@PathVariable Long id, Model model) {
+        Optional<Product> product = service.findById(id);
+        System.out.println("product: " + product);
+        if (product.isPresent()) {
+            model.addAttribute("product", product);
+            return "update";
+        } else {
+            return "redirect:/";
+        }
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute("product") Product product) {
+        service.saveProducts(product);
+        return "redirect:/";
+    }
+
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute("product") Product product) {
+        service.saveProducts(product);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        service.deleteProduct(id);
+        return "redirect:/";
     }
 }
--- backend/tugas-12/gen-24-java-spring-framework/src/main/java/com/bagus/gen_24_java_spring_framework/service/ProductService.java
+++ backend/tugas-12/gen-24-java-spring-framework/src/main/java/com/bagus/gen_24_java_spring_framework/service/ProductService.java
@@ -2,28 +2,36 @@ package com.bagus.gen_24_java_spring_framework.service;
 
 import com.bagus.gen_24_java_spring_framework.entity.Product;
 import com.bagus.gen_24_java_spring_framework.entity.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ProductService {
     private final ProductRepository repository;
 
     private List<Product> products = new ArrayList<>();
 
-    public void saveProducts() {
-        repository.save(new Product(1L, "Tahu", "Makanan", 5000.0, 50L));
-        repository.save(new Product(2L, "Tempe", "Makanan", 3000.0, 55L));
-        repository.save(new Product(3L, "Aqua", "Minuman", 2800.0, 100L));
-        repository.save(new Product(4L, "Golda Coffee", "Minuman", 3000.0, 80L));
-        repository.save(new Product(5L, "Bodrex", "Obat", 10000.0, 20L));
+    public void saveProducts(Product product) {
+        repository.save(product);
+    }
+
+    public Optional<Product> findById(Long id) {
+        return repository.findById(id);
     }
 
     public List<Product> getAllProducts() {
         return repository.findAll();
+    }
+
+    public void deleteProduct(Long id) {
+        repository.deleteById(id);
     }
 }

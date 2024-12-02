923e0c3ae8a70f43834cd976997d02bbf5efdf80
bagusutom0 <bagussajiwoutomo12@gmail.com>
2024-12-02 08:02:00 +0700

    tugas 15_bagus

--- backend/tugas-15/gen-24-java-springboot-pos/src/main/java/com/bagus/gen_24_java_springboot_pos/service/scheduled/ScheduledTaskService.java
+++ backend/tugas-15/gen-24-java-springboot-pos/src/main/java/com/bagus/gen_24_java_springboot_pos/service/scheduled/ScheduledTaskService.java
@@ -7,12 +7,12 @@ import java.time.LocalDateTime;
 
 @Service
 public class ScheduledTaskService {
-    @Scheduled(cron = "0 0 9 * * MON-FRI")
+    @Scheduled(cron = "0 0 9 ? * MON-FRI")
     public void sarapan() {
         System.out.println("Pukul 9 waktu sarapan!!");
     }
 
-    @Scheduled(cron = "0 0 15 * * MON-FRI")
+    @Scheduled(cron = "0 0 15 ? * MON-FRI")
     public void makanSore() {
         System.out.println("Pukul 15 waktu makan sore!!");
     }

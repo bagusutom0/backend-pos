807ba1abeb3263f15927b340fb1d6f1f78e764f1
bagusutom0 <bagussajiwoutomo12@gmail.com>
2024-12-05 14:01:08 +0700

    tugas 17-bagus

---
+++ backend/tugas-18/gen-24-java-springboot-pos-producer-kafka/src/main/java/com/bagus/gen_24_java_springboot_pos_producer_kafka/Gen24JavaSpringbootPosProducerKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.bagus.gen_24_java_springboot_pos_producer_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gen24JavaSpringbootPosProducerKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gen24JavaSpringbootPosProducerKafkaApplication.class, args);
+	}
+
+}
---
+++ backend/tugas-18/gen-24-java-springboot-pos-producer-kafka/src/test/java/com/bagus/gen_24_java_springboot_pos_producer_kafka/Gen24JavaSpringbootPosProducerKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagus.gen_24_java_springboot_pos_producer_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gen24JavaSpringbootPosProducerKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
